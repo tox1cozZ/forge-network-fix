1477927ac845c35f4e0874ffc5e1645f75ebfc8e
tox1cozZ <Agravaine25@yandex.ru>
2023-02-28 17:00:36 +0200

    Initial commit

--- src/main/java/io/github/tox1cozz/forgenetworkfix/asm/CoremodPlugin.java
+++ src/main/java/io/github/tox1cozz/forgenetworkfix/asm/CoremodPlugin.java
@@ -1,12 +1,16 @@
 package io.github.tox1cozz.forgenetworkfix.asm;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
 import io.github.tox1cozz.mixinbooterlegacy.IEarlyMixinLoader;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@Name("ForgeNetworkFix")
+@MCVersion("1.7.10")
 public class CoremodPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
